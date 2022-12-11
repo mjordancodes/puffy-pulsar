8257b26fb023bee62bb2ac8a9093c80b1d2cc228
Jordan Levine <hi@mjordan.codes>
2022-12-11 16:12:48 -0600

    throwing things out

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "mJordan Codes";
+export const AUTHOR_NAME = "Jordan Levine";
+export const SITE_DESCRIPTION = `Digital Garden of ${AUTHOR_NAME}`;
